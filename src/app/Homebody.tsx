b02333e22534fd709562684fbbc6272a1ec1f222
stephaniemukami <stephaniekariuki19@gmail.com>
2024-03-23 23:21:10 +0300

    added landing page

--- src/app/Homebody.tsx
+++ src/app/Homebody.tsx
@@ -1,31 +1,28 @@
 import React from "react";
-import Image from "next/image";
 
-type Props = {};
-
-function Homebody({}: Props) {
+function Homebody() {
   return (
-    <div className="flex-body lg:flex bg-stone-50 flex w-screen h-screen">
-      <div className="text-body flex-4 p-10">
+    <div className="flex-body border border-black lg:flex bg-stone-50 flex-col sm:flex-col md:flex-col lg:flex-row w-screen h-full">
+      <div className="text-body flex-4 p-10 text-center md:text-left sm:text-center">
         <h2 className="text-8xl mb-3 text-brand-blue">Medbot</h2>
         <h2 className="text-3xl m-1 mb-2">Easy and accurate diagnosis</h2>
         <h4 className="text-base mb-3">
-          A medical chat-bot that  access to health information and 
+          A medical chat-bot that provides access to health information and 
           assistance in diagnosing respiratory diseases.
         </h4>
-        <h5 className="text-sm text-brand-blue mb-3">Quick Guide</h5>
+        <h5 className="text-sm text-brand-blue mb-6 ">Quick Guide</h5>
 
         <button className="w-32 h-19 relative p-1 rounded-lg bg-brand-blue text-white transition-transform transform-gpu hover:translate-y-1">
             Get Started
         </button>
 
       </div>
-      <div className="img-body">
+      <div className="img-body  relative bottom-6">
         <img
           src="./lung-redone.png"
           srcSet="./lung-redone.png 640w, ./Lungs-smallest.png 320w"
           alt="landing-icon"
-          className=" relative right-4 m-2 h-15 w-15"
+          className=" relative right-4 m-2 h-15 w-15 w-full h-full object-cover "
         />
       </div>
     </div>
---
+++ src/app/SignupBody.tsx
@@ -0,0 +1,141 @@
+"use client"
+import { useState } from "react";
+
+export default function SignupBody() {
+  const [data, setData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: ""
+  });
+
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen border border-red-800 bg-cover"  style={{backgroundImage: 'url("./medics1.jpg")'}}>
+
+        <div className="w-full sm:max-w-sm p-4 bg-light-grey rounded-lg bg-opacity-35">
+
+          <h2 className="text-2xl font-bold text-white text-center">
+            Create your account!
+          </h2>
+
+          <form className="mt-4 space-y-6">
+
+            <div className=" p-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                First Name
+              </label>
+
+              <div className="mt-1">
+                <input
+                placeholder=" e.g. John"
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  value={data.email}
+                  onChange={(e) =>
+                    setData({ ...data, email: e.target.value })
+                  }
+                  required
+                  className="h-6 block w-full rounded-sm border-gray-300 shadow-sm placeholder-gray-400 sm:text-sm focus:border-brand-blue focus:outline-none focus:ring-1 focus:ring-brand-blue"
+                />
+              </div>
+            </div>
+
+            <div className=" p-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Last Name
+              </label>
+
+              <div className="mt-1">
+                <input
+                placeholder=" e.g. Doe"
+
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  value={data.email}
+                  onChange={(e) =>
+                    setData({ ...data, email: e.target.value })
+                  }
+                  required
+                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder-gray-400 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div className="p-2">
+
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Email address
+              </label>
+
+              <div className="mt-1">
+                <input
+                placeholder=" e.g. johndoe@gmail.com"
+
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  value={data.email}
+                  onChange={(e) =>
+                    setData({ ...data, email: e.target.value })
+                  }
+                  required
+                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder-gray-400 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div className="p-2">
+
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Password
+              </label>
+
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  value={data.password}
+                  onChange={(e) =>
+                    setData({ ...data, password: e.target.value })
+                  }
+                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 placeholder-gray-400 sm:text-sm"
+                />
+              </div>
+              
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className=" transition-transform transform-gpu hover:translate-y-1  w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-semibold text-white bg-brand-blue"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
