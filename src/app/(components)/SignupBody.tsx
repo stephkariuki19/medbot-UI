0884b6294aea3cf6c9f4b0ad75b8dbeb5a10084c
stephaniemukami <stephaniekariuki19@gmail.com>
2024-03-25 12:06:59 +0300

    started new auth routes

--- src/app/(components)/SignupBody.tsx
+++ src/app/(components)/SignupBody.tsx
@@ -1,29 +1,41 @@
-"use client"
+"use client";
 import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function SignupBody() {
   const [data, setData] = useState({
     first_name: "",
     last_name: "",
     email: "",
-    password: ""
+    password: "",
   });
+
+  const registerUser = async (e: { preventDefault: () => void }) => {
+    e.preventDefault()
+    console.log(data)
+    axios.post('/api/registerApi', data)
+    .then(() => setData({first_name:'',last_name:'', email:'',password:''}))
+    .then(() => toast.success('Registration is successful!'))
+    .catch(() => toast.error('Something went wrong!'))
+ }
+
 
   return (
     <>
-      <div className="flex justify-center items-center min-h-screen border border-red-800 bg-cover"  style={{backgroundImage: 'url("./medics1.jpg")'}}>
-
+      <div
+        className="flex justify-center items-center min-h-screen border border-red-800 bg-cover"
+        style={{ backgroundImage: 'url("./medics1.jpg")' }}
+      >
         <div className="w-full sm:max-w-sm p-4 bg-light-grey rounded-lg bg-opacity-35">
-
           <h2 className="text-2xl font-bold text-white text-center">
             Create your account!
           </h2>
 
-          <form className="mt-4 space-y-6">
-
+          <form className="mt-4 space-y-6" onSubmit={registerUser}>
             <div className=" p-2">
               <label
-                htmlFor="email"
+                htmlFor="first_name"
                 className="block text-sm font-medium leading-6 text-white"
               >
                 First Name
@@ -31,15 +43,12 @@ export default function SignupBody() {
 
               <div className="mt-1">
                 <input
-                placeholder=" e.g. John"
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  value={data.email}
-                  onChange={(e) =>
-                    setData({ ...data, email: e.target.value })
-                  }
+                  placeholder=" e.g. John"
+                  id="first_name"
+                  name="first_name"
+                  type="text"
+                  value={data.first_name}
+                  onChange={(e) => setData({ ...data, first_name: e.target.value })}
                   required
                   className="h-6 block w-full rounded-sm border-gray-300 shadow-sm placeholder-gray-400 sm:text-sm focus:border-brand-blue focus:outline-none focus:ring-1 focus:ring-brand-blue"
                 />
@@ -48,7 +57,7 @@ export default function SignupBody() {
 
             <div className=" p-2">
               <label
-                htmlFor="email"
+                htmlFor="text"
                 className="block text-sm font-medium leading-6 text-white"
               >
                 Last Name
@@ -56,25 +65,19 @@ export default function SignupBody() {
 
               <div className="mt-1">
                 <input
-                placeholder=" e.g. Doe"
-
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  value={data.email}
-                  onChange={(e) =>
-                    setData({ ...data, email: e.target.value })
-                  }
+                  placeholder=" e.g. Doe"
+                  id="last_name"
+                  name="last_name"
+                  type="last_name"
+                  value={data.last_name}
+                  onChange={(e) => setData({ ...data, last_name: e.target.value })}
                   required
                   className="h-6 block w-full rounded-sm border-gray-300 shadow-sm placeholder-gray-400 sm:text-sm focus:border-brand-blue focus:outline-none focus:ring-1 focus:ring-brand-blue"
-
                 />
               </div>
             </div>
 
             <div className="p-2">
-
               <label
                 htmlFor="email"
                 className="block text-sm font-medium leading-6 text-white"
@@ -84,25 +87,20 @@ export default function SignupBody() {
 
               <div className="mt-1">
                 <input
-                placeholder=" e.g. johndoe@gmail.com"
-
+                  placeholder=" e.g. johndoe@gmail.com"
                   id="email"
                   name="email"
                   type="email"
                   autoComplete="email"
                   value={data.email}
-                  onChange={(e) =>
-                    setData({ ...data, email: e.target.value })
-                  }
+                  onChange={(e) => setData({ ...data, email: e.target.value })}
                   required
                   className="h-6 block w-full rounded-sm border-gray-300 shadow-sm placeholder-gray-400 sm:text-sm focus:border-brand-blue focus:outline-none focus:ring-1 focus:ring-brand-blue"
-
                 />
               </div>
             </div>
 
             <div className="p-2">
-
               <label
                 htmlFor="password"
                 className="block text-sm font-medium leading-6 text-white"
@@ -122,10 +120,8 @@ export default function SignupBody() {
                     setData({ ...data, password: e.target.value })
                   }
                   className="h-6 block w-full rounded-sm border-gray-300 shadow-sm placeholder-gray-400 sm:text-sm focus:border-brand-blue focus:outline-none focus:ring-1 focus:ring-brand-blue"
-
                 />
               </div>
-              
             </div>
 
             <div>
